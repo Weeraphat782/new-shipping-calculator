9cc3032c4c86bf4ed6c127a6facb6adff480e6c8
Weeraphat782 <Weeraphat.Issaraphon1@gmail.com>
2025-01-28 11:54:34 +0700

    Update deployment configuration

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
+    images: {
+      domains: ['placeholder.com'],
+    },
+    webpack: (config) => {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
+      return config;
+    },
   }
-  
-  module.exports = nextConfig
